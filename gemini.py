ec42030a13251e1f1986ed07cc36ad1e995ed463
SwastikKhan <121816379+SwastikKhan@users.noreply.github.com>
2024-07-11 20:44:19 +0530

    gemini.py

--- gemini.py
+++ gemini.py
@@ -19,5 +19,4 @@ def get_response_from_model(code, API_KEY):
     response = model.generate_content(prompt_parts)
     output=response.text
     return output
-#code='''The aroma of freshly baked bread hung heavy in the air, a warm invitation wafting from the little bakery tucked away on the corner. Inside, sunlight streamed through the window, casting a golden glow on flour-dusted shelves overflowing with crusty baguettes and flaky croissants. A gentle symphony of sounds filled the space - the rhythmic thump of dough being kneaded, the satisfying sizzle of pastries meeting the hot oven, and the cheerful chatter of customers exchanging greetings. It was a haven for bread enthusiasts, a place where simple ingredients were transformed into edible works of art, promising a delightful sensory experience with every bite.'''
-#print(get_response_from_model(code,GOOGLE_API_KEY))
+
